b50e3646f62cd18d833df8b2f3d6f95ec3061059
Roman Kurlykin <CoulrophobicClown@gmail.com>
2020-07-27 15:29:57 +0300

    Added basic CSS

--- movie_theater.py
+++ movie_theater.py
@@ -1,6 +1,6 @@
 import config #contains DB config and secret key
 from flask import Flask, render_template, request, session
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from functools import wraps
 
 app = Flask(__name__)
@@ -17,36 +17,57 @@ def movies_mgmt() -> "html":
     #Delete requests only contain movie id values as dictionary keys,
     #so if there are no other keys, then it is a delete request
     if request.method == "POST":
-        if "rel_year" not in request.form.keys():
-            #print("Deleting movies")
-            db_del_movie(request)
-        elif "id" in request.form.keys():
-            #print("Editing a movie")
-            db_edit_movie(request)
-        else:
-            #print("Adding a movie")
-            db_add_movie(request)
+        try:
+            if "rel_year" not in request.form.keys():
+                #print("Deleting movies")
+                db_del_movie(request)
+            elif "id" in request.form.keys():
+                #print("Editing a movie")
+                db_edit_movie(request)
+            else:
+                #print("Adding a movie")
+                db_add_movie(request)
+        except ConnectionError as err:
+            print("Database connection error:", str(err))
+        except CredentialsError as err:
+            print("Database authentication error:", str(err))
+        except SQLError as err:
+            print("Something is worng with the SQL request:", str(err))
+        except Exception as err:
+            print("Error:", str(err))
     _SQL = """select id, rel_year, title_ru, title_orig, duration,
               descr, genre, age_restr, director, cast
               from movies
               order by rel_year, title_ru, title_orig"""
-    contents = db_request(_SQL)
-    title = "Список доступных фильмов"
-    col_titles = ("Год выпуска", "Название", "Оригинальное название",
-                  "Продолжительность", "Описание", "Жанр", 
-                  "Возрастное ограничение", "Режиссёр", "В ролях")
-    return render_template("db_mgmt.html",
-                           page_title=title,
-                           action1="/movies_add",
-                           action2="/movies_edit",
-                           action3="/movies_delete",
-                           col_titles=col_titles,
-                           data=contents)
+    try:
+        contents = db_request(_SQL)
+        title = "Список доступных фильмов"
+        col_titles = ("Год", "Название", "Оригинальное название",
+                      "Продолжит. (мин)", "Описание", "Жанр", 
+                      "Возрастное огранич.", "Режиссёр", "В ролях")
+        return render_template("db_mgmt.html",
+                               page_title=title,
+                               action1="/movies_add",
+                               action2="/movies_edit",
+                               action3="/movies_delete",
+                               col_titles=col_titles,
+                               data=contents)
+    except ConnectionError as err:
+        print("Database connection error:", str(err))
+    except CredentialsError as err:
+        print("Database authentication error:", str(err))
+    except SQLError as err:
+        print("Something is worng with the SQL request:", str(err))
+    except Exception as err:
+        print("Error:", str(err))
+    return "Error"
 
 @app.route("/movies_add", methods=["GET", "POST"])
 def add_movie() -> "html":
     title = "Добавить новый фильм"
-    return render_template("movies_add.html", page_title=title)
+    return render_template("movies_add.html", 
+                           page_title=title, 
+                           action1="/movies")
 
 def db_add_movie(req: "flask_request") -> None:
     """Add information about a movie to the database"""
@@ -72,11 +93,22 @@ def edit_movie() -> "html":
     movie_id = list(request.form.keys())[list(request.form.values()).index("id")]
     _SQL = """select * from movies
               where id = %s"""
-    contents = db_request(_SQL, movie_id)
-    title = "Редактирование фильмов"
-    return render_template("edit.html", 
-                           page_title=title,
-                           data=contents)
+    try:
+        contents = db_request(_SQL, movie_id)
+        title = "Редактирование фильмов"
+        return render_template("edit.html", 
+                               page_title=title,
+                               data=contents,
+                               action1="/movies")
+    except ConnectionError as err:
+        print("Database connection error:", str(err))
+    except CredentialsError as err:
+        print("Database authentication error:", str(err))
+    except SQLError as err:
+        print("Something is worng with the SQL request:", str(err))
+    except Exception as err:
+        print("Error:", str(err))
+    return "Error"
 
 def db_edit_movie(req: "flask_request") -> None:
     with UseDatabase(app.config["dbconfig"]) as cursor:
@@ -111,13 +143,23 @@ def delete_movie() -> "html":
               where """
     _SQL, req_keys = add_id(_SQL, request)
     _SQL += """ order by rel_year"""
-    contents = db_request(_SQL, *req_keys)
-    title = "Удаление фильмов"
-    return render_template("delete.html", 
-                           page_title=title,
-                           obj="фильмы",
-                           action1="/movies",
-                           data=contents)
+    try:
+        contents = db_request(_SQL, *req_keys)
+        title = "Удаление фильмов"
+        return render_template("delete.html", 
+                               page_title=title,
+                               obj="фильмы",
+                               action1="/movies",
+                               data=contents)
+    except ConnectionError as err:
+        print("Database connection error:", str(err))
+    except CredentialsError as err:
+        print("Database authentication error:", str(err))
+    except SQLError as err:
+        print("Something is worng with the SQL request:", str(err))
+    except Exception as err:
+        print("Error:", str(err))
+    return "Error"
 
 def db_del_movie(req: "flask_request") -> None:
     with UseDatabase(app.config["dbconfig"]) as cursor:
@@ -134,21 +176,32 @@ def cin_halls_mgmt() -> "html":
         else:
             db_add_cin_hall(request)
     _SQL = """select * from cin_halls;"""
-    contents = db_request(_SQL)
-    title = "Кинозалы"
-    col_titles = ("Название", "Расположение")
-    return render_template("db_mgmt.html",
-                           page_title=title,
-                           action1="/cinema_halls_add",
-                           action2="/cinema_halls_edit",
-                           action3="/cinema_halls_delete", 
-                           col_titles=col_titles,
-                           data=contents)
+    try:
+        contents = db_request(_SQL)
+        title = "Кинозалы"
+        col_titles = ("Название", "Расположение")
+        return render_template("db_mgmt.html",
+                               page_title=title,
+                               action1="/cinema_halls_add",
+                               action2="/cinema_halls_edit",
+                               action3="/cinema_halls_delete", 
+                               col_titles=col_titles,
+                               data=contents)
+    except ConnectionError as err:
+        print("Database connection error:", str(err))
+    except CredentialsError as err:
+        print("Database authentication error:", str(err))
+    except SQLError as err:
+        print("Something is worng with the SQL request:", str(err))
+    except Exception as err:
+        print("Error:", str(err))
+    return "Error"
 
 @app.route("/cinema_halls_add", methods=["GET", "POST"])
 def add_cin_hall() -> "html":
     return render_template("cin_halls_add.html", 
-                           page_title="Add a cinema hall")
+                           page_title="Добавить кинозал",
+                           action1="/cinema_halls")
 
 def db_add_cin_hall(req: "flask_request") -> None:
     with UseDatabase(app.config["dbconfig"]) as cursor: 
@@ -190,13 +243,23 @@ def del_cin_hall() -> "html":
               where """
     _SQL, req_keys = add_id(_SQL, request)
     _SQL += """ order by name"""
-    contents = db_request(_SQL, *req_keys)
-    title = "Удаление кинозалов"
-    return render_template("delete.html", 
-                           page_title=title,
-                           obj="кинозалы",
-                           action1="/cinema_halls",
-                           data=contents)
+    try:
+        contents = db_request(_SQL, *req_keys)
+        title = "Удаление кинозалов"
+        return render_template("delete.html", 
+                               page_title=title,
+                               obj="кинозалы",
+                               action1="/cinema_halls",
+                               data=contents)
+    except ConnectionError as err:
+        print("Database connection error:", str(err))
+    except CredentialsError as err:
+        print("Database authentication error:", str(err))
+    except SQLError as err:
+        print("Something is worng with the SQL request:", str(err))
+    except Exception as err:
+        print("Error:", str(err))
+    return "Error"
 
 def db_del_cin_hall(req: "flask_request") -> None:
     with UseDatabase(app.config["dbconfig"]) as cursor:
